6494703d60f073cbfaed707274b23897b8d3c802
Rustem <luxevp@gmail.com>
2023-11-16 12:51:13 +0300

    first ex was done

--- workfile.js
+++ workfile.js
@@ -21,6 +21,8 @@
 */
 
 const numberOfFilms = prompt('Сколько фильмов уже посмотрели?', '');
+const lastWatchingMovie = prompt('Один из последних просмотренных фильмов?', '');
+const lastWatchingMovieMark = +prompt('Один из последних просмотренных фильмов?', 'Только цифры от 0 до 10');
 const personalOfMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -28,3 +30,5 @@ const personalOfMovieDB = {
     genres: [],
     privat: false,
 };
+personalOfMovieDB.movies[lastWatchingMovie] = lastWatchingMovieMark;
+console.log(personalOfMovieDB);
